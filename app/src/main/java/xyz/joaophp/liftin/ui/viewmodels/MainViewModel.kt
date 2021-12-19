f0f5c16c0aa5709419920ba63157a9047f4553bd
joaophp <hahmeyerjoao@gmail.com>
2021-12-19 17:09:01 -0300

    Refactored MainViewModel a bit

--- app/src/main/java/xyz/joaophp/liftin/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/xyz/joaophp/liftin/ui/viewmodels/MainViewModel.kt
@@ -3,19 +3,19 @@ package xyz.joaophp.liftin.ui.viewmodels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 import xyz.joaophp.liftin.ui.state.AppState
 
 // App wide ViewModel for managing app state
 class MainViewModel : ViewModel() {
 
-    private val appStateFlow = MutableSharedFlow<AppState>()
-    val appState: SharedFlow<AppState> = appStateFlow
+    private val _appState = MutableSharedFlow<AppState>()
+    val appState = _appState.asSharedFlow()
 
     fun updateState(state: AppState) {
         viewModelScope.launch {
-            appStateFlow.emit(state)
+            _appState.emit(state)
         }
     }
 }
