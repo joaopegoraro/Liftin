51963a561a149e67caa9e0da0b74d188dd84225c
joaophp <hahmeyerjoao@gmail.com>
2021-12-19 17:52:34 -0300

    Added navigation

--- app/src/main/java/xyz/joaophp/liftin/ui/state/AppState.kt
+++ app/src/main/java/xyz/joaophp/liftin/ui/state/AppState.kt
@@ -4,8 +4,8 @@ import xyz.joaophp.liftin.data.models.Exercise
 import xyz.joaophp.liftin.data.models.User
 import xyz.joaophp.liftin.data.models.Workout
 
-sealed class AppState(user: User) {
-    class InAuth(user: User) : AppState(user)
+sealed class AppState(user: User?) {
+    class InAuth(user: User?) : AppState(user)
     class InHome(user: User) : AppState(user)
     class InAddWorkout(user: User) : AppState(user)
     class InWorkout(user: User, workout: Workout) : AppState(user)
--- app/src/main/java/xyz/joaophp/liftin/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/xyz/joaophp/liftin/ui/viewmodels/MainViewModel.kt
@@ -2,16 +2,16 @@ package xyz.joaophp.liftin.ui.viewmodels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import xyz.joaophp.liftin.ui.state.AppState
 
 // App wide ViewModel for managing app state
 class MainViewModel : ViewModel() {
 
-    private val _appState = MutableSharedFlow<AppState>()
-    val appState = _appState.asSharedFlow()
+    private val _appState = MutableStateFlow<AppState?>(null)
+    val appState = _appState.asStateFlow()
 
     fun updateState(state: AppState) {
         viewModelScope.launch {
