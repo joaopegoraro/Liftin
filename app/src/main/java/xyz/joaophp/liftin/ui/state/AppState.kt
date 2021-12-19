be789c0ffe5f056262ab905d4d3720efc8d9c8dd
joaophp <hahmeyerjoao@gmail.com>
2021-12-19 16:59:16 -0300

    Implemented AppState

---
+++ app/src/main/java/xyz/joaophp/liftin/ui/state/AppState.kt
@@ -0,0 +1,14 @@
+package xyz.joaophp.liftin.ui.state
+
+import xyz.joaophp.liftin.data.models.Exercise
+import xyz.joaophp.liftin.data.models.User
+import xyz.joaophp.liftin.data.models.Workout
+
+sealed class AppState(user: User) {
+    class InAuth(user: User) : AppState(user)
+    class InHome(user: User) : AppState(user)
+    class InAddWorkout(user: User) : AppState(user)
+    class InWorkout(user: User, workout: Workout) : AppState(user)
+    class InAddExercise(user: User, workout: Workout) : AppState(user)
+    class InExercise(user: User, exercise: Exercise) : AppState(user)
+}
