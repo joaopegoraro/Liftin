02198d160b63effecde57f0a9bc5bb595b532e89
joaophp <hahmeyerjoao@gmail.com>
2021-12-19 19:23:07 -0300

    Setup HomeFragment

--- app/src/main/java/xyz/joaophp/liftin/ui/fragments/HomeFragment.kt
+++ app/src/main/java/xyz/joaophp/liftin/ui/fragments/HomeFragment.kt
@@ -7,8 +7,10 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
-import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.collect
 import xyz.joaophp.liftin.MainActivity
 import xyz.joaophp.liftin.R
 import xyz.joaophp.liftin.data.models.User
@@ -27,10 +29,17 @@ class HomeFragment : Fragment() {
     private lateinit var user: User
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Check authentication
         viewModel.getUser().fold(
             ifError = { failure -> handleFailure(failure) },
             ifSuccess = { user -> appViewModel.updateState(AppState.InHome(user)) }
         )
+
+        // Handle state
+        lifecycleScope.launchWhenStarted {
+            appViewModel.appState.collect { state -> handleState(state) }
+        }
+
         super.onCreate(savedInstanceState)
     }
 
@@ -39,17 +48,25 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setUpCollectors()
         return super.onCreateView(inflater, container, savedInstanceState)
     }
 
-    private fun handleFailure(failure: Failure) {
-        when (failure) {
-            is AuthFailure.NoCurrentUser -> navigateToAuthFragment()
-            is AuthFailure.CantRetrieveUser -> {
-                (activity as MainActivity)
-                    .showToast("There was a problem retrieving authentication information")
-                navigateToAuthFragment()
-            }
+    // Handle flows
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    private fun setUpCollectors() {
+        lifecycleScope.launchWhenStarted {
+            viewModel.getWorkouts(user)
+        }
+    }
+
+    // App state handling
+
+    private fun handleState(state: AppState?) {
+        when (state) {
+            is AppState.InHome -> user = state.user
+            else -> Unit
         }
     }
 
@@ -69,4 +86,18 @@ class HomeFragment : Fragment() {
         appViewModel.updateState(AppState.InWorkout(user, workout))
         findNavController().navigate(R.id.action_homeFragment_to_workoutFragment)
     }
+
+    // Error handling
+
+    private fun handleFailure(failure: Failure) {
+        when (failure) {
+            is AuthFailure.NoCurrentUser -> navigateToAuthFragment()
+            is AuthFailure.CantRetrieveUser -> {
+                (activity as MainActivity)
+                    .showToast("There was a problem retrieving authentication information")
+                navigateToAuthFragment()
+            }
+        }
+    }
+
 }
--- app/src/main/java/xyz/joaophp/liftin/ui/state/AppState.kt
+++ app/src/main/java/xyz/joaophp/liftin/ui/state/AppState.kt
@@ -6,7 +6,7 @@ import xyz.joaophp.liftin.data.models.Workout
 
 sealed class AppState(user: User?) {
     class InAuth(user: User?) : AppState(user)
-    class InHome(user: User) : AppState(user)
+    class InHome(val user: User) : AppState(user)
     class InAddWorkout(user: User) : AppState(user)
     class InWorkout(user: User, workout: Workout) : AppState(user)
     class InAddExercise(user: User, workout: Workout) : AppState(user)
