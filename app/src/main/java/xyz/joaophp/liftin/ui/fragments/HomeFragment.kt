bee1761162e39d78015af60d7346cf961d76f365
joaophp <hahmeyerjoao@gmail.com>
2021-12-20 16:30:43 -0300

    Made progress with HomeFragment

--- app/src/main/java/xyz/joaophp/liftin/ui/fragments/HomeFragment.kt
+++ app/src/main/java/xyz/joaophp/liftin/ui/fragments/HomeFragment.kt
@@ -1,21 +1,20 @@
 package xyz.joaophp.liftin.ui.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.collect
-import xyz.joaophp.liftin.MainActivity
 import xyz.joaophp.liftin.R
 import xyz.joaophp.liftin.data.models.User
 import xyz.joaophp.liftin.data.models.Workout
+import xyz.joaophp.liftin.databinding.FragmentHomeBinding
 import xyz.joaophp.liftin.ui.state.AppState
 import xyz.joaophp.liftin.ui.viewmodels.AppViewModel
 import xyz.joaophp.liftin.ui.viewmodels.HomeViewModel
@@ -25,15 +24,24 @@ import xyz.joaophp.liftin.utils.failures.Failure
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
 
+    private var binding: FragmentHomeBinding? = null
+
     private val appViewModel: AppViewModel by activityViewModels()
     private val viewModel: HomeViewModel by viewModels()
 
     private lateinit var user: User
 
+    // Lifecycle methods
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
         // Check authentication
         viewModel.getUser().fold(
-            ifError = { failure -> handleFailure(failure) },
+            ifError = { failure ->
+                handleFailure(failure)
+                navigateToAuthFragment()
+            },
             ifSuccess = { user -> appViewModel.updateState(AppState.InHome(user)) }
         )
 
@@ -41,8 +49,6 @@ class HomeFragment : Fragment() {
         lifecycleScope.launchWhenStarted {
             appViewModel.appState.collect { state -> handleState(state) }
         }
-
-        super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(
@@ -50,8 +56,48 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        binding?.apply {
+            fab.setOnClickListener { navigateToAddWorkoutFragment() }
+        }
+
         setUpCollectors()
-        return super.onCreateView(inflater, container, savedInstanceState)
+
+        setHasOptionsMenu(true)
+        return binding?.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_home, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.logout_icon -> signOut()
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    // Action methods
+
+    private fun signOut(): Boolean {
+
+        // Show loading animation
+        binding?.loading?.show()
+        binding?.root?.alpha = 0.5f // lowers opacity during loading
+
+        viewModel.signOut().fold(
+            ifError = { failure -> handleFailure(failure) },
+            ifSuccess = { navigateToAuthFragment() }
+        )
+
+        // Hide loading animation
+        binding?.root?.alpha = 1f // opacity goes back to normal
+        binding?.loading?.hide()
+
+        return true
     }
 
     // Handle flows
@@ -93,13 +139,18 @@ class HomeFragment : Fragment() {
 
     private fun handleFailure(failure: Failure) {
         when (failure) {
-            is AuthFailure.NoCurrentUser -> navigateToAuthFragment()
+            is AuthFailure.NoCurrentUser -> Unit
             is AuthFailure.CantRetrieveUser -> {
-                (activity as MainActivity)
-                    .showToast("There was a problem retrieving authentication information")
+                displayError("There was a problem retrieving authentication information")
                 navigateToAuthFragment()
             }
+            else -> displayError("An unexpected error has occured\n" + failure.e)
         }
     }
 
+    private fun displayError(message: String) {
+        view?.let {
+            Snackbar.make(it, message, Snackbar.LENGTH_LONG).show()
+        }
+    }
 }
